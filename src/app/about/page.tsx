83f4c564a45f066dd21538cb4e3a24e4e87b6337
prajjwal81 <prajjwalverma8126@gmail.com>
2024-07-01 12:42:30 +0530

    feat/some changes in content and name

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -11,20 +11,20 @@ const About = () => {
           </h1>
           <h1>Welcome to Kumar Lakshay and Associates</h1>
           <p>
-            While you are facing any sort of legal questioning, you might not be
-            aware of where to start your journey from. Indeed, it is a daunting
-            task to evaluate the legal issues along with choosing the perfect
-            lawyer to handle your legal matters as per your requirements.
-            Searching for a lawyer is an easy task, but searching for the right
+            While you are facing any sort of a legal questioning, you must not
+            be aware from where to start your journey from. Indeed it is a
+            daunting task to evaluate the legal issues along with choosing the
+            perfect lawyer to handle your legal things as per your requirements.
+            Searching for a lawyer is an easy task but searching for the Right
             lawyer is what we can serve you right here to give you the One Stop
             Solution with our team of experts.
           </p>
 
           <p>
-            We, at Kumar Lakshay and Associates, are here to assist you with
-            your personal and financial stakes. Be it corporate or general
-            clients, since it is not that easy to gather the facts, we help you
-            out with every bit of it.
+            We here at Kumar Lakshay and associates are right here to assist you
+            with your personal and your financial stakes. Be it corporate or the
+            general clients, since it is not that easy to gather the facts we
+            help you out with every bit of it.
           </p>
 
           <h2>Our USP</h2>
@@ -46,9 +46,9 @@ const About = () => {
               resolving disputes, and helping to achieve their business goals.
             </li>
             <li>
-              Whether a small business in an emerging industry, a diversified
-              multinational corporation, or an individual, each of our clients
-              represents an important relationship with us to whom we dedicate
+              ⁠Whether a small business in an emerging industry or a diversified
+              multinational corporation or an individual, each of our clients
+              represent an important relationship with us to whom we dedicate
               our best attention and service.
             </li>
           </ul>
@@ -61,7 +61,7 @@ const About = () => {
 
           <p>
             Our objective is to provide top-notch legal services. We aim to
-            achieve favorable outcomes for our clients through expert
+            achieve favourable outcomes for our clients through expert
             representation in court and efficient conflict resolution methods
             like mediation and arbitration. Our commitment is to uphold justice,
             integrity, and professionalism in all we do.
